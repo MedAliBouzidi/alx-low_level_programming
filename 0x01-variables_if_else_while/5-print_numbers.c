ef540ad5e254cbf6d3ace76f61575febf537aaf0
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-17 08:55:32 -0700

    fix task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	int digit;
+	char digit;
 
-	for (digit = 0; digit < 10; digit++)
+	for (digit = '0'; digit <= '9'; digit++)
 		putchar(digit);
 	putchar('\n');
 	return (0);
