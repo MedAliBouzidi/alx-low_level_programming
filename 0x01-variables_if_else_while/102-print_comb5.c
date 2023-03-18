0c68e63abb95cf3ceeaf51f64a0909d3d56ad773
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-17 19:17:29 -0700

    fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,19 @@ int main(void)
 	short b;
 	short c;
 	short d;
+	short comb1;
+	short comb2;
 
 	for (a = 0; a < 10; a++)
 	for (b = 0; b < 9; b++)
 	for (c = 0; c < 10; c++)
 	for (d = 0; d < 10; d++)
-		if (a + '0' + b != c + '0' + d && a + '0' + b < c + '0' + d)
+	{
+		comb1 = a * 10 + b;
+		comb2 = c * 10 + d;
+		if (comb1 != comb2 && comb1 < comb2)
 		{
+
 			putchar(a + '0');
 			putchar(b + '0');
 			putchar(' ');
@@ -29,6 +35,7 @@ int main(void)
 				putchar(' ');
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
