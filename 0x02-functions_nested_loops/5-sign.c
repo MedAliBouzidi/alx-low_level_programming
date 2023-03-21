0312b7c9d0a8a9b77657f06e9857bd6b7db58a96
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 09:53:36 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number 
+ *
+ * @c: The character ascii code
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
