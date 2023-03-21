dfb2ab1f66e8ef1dd6d71f2ccde6b52452f60be9
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 15:24:09 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,13 +7,15 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int a = 0, b, c;
 
 	if (n > 15 || n < 0)
 		return;
-	for (a = 0; a <= n; a++)
+	do
 	{
 		_putchar(48);
+		if (n == 0)
+			return;
 		for (b = 0; b <= n; b++)
 		{
 			c = a * b;
@@ -24,12 +26,18 @@ void print_times_table(int n)
 				_putchar(32);
 				_putchar(c + 48);
 			}
-			else
+			else if (c <= 99)
 			{
 				_putchar((c / 10) + 48);
 				_putchar((c % 10) + 48);
 			}
+			else
+			{
+				_putchar((c / 100) + 48);
+				_putchar(((c / 100) / 10) + 48);
+				_putchar(((c / 100) % 10) + 48);
+			}
 		}
 		_putchar('\n');
-	}
+	} while (a < n)
 }
