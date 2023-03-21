2dfb6232bb058549475d61a88aaa60128907c076
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 16:23:17 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	short i;
+	long f = 1, s = 2, n = f + s, total = 0;
+
+	while (n < 4000000)
+	{
+		total += n;
+		f = s;
+		s = n;
+		n = f + s;
+	}
+	printf("%ld\n", total);
+	return (0);
+}
