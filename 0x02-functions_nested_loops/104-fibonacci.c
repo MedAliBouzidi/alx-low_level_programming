487341d7e65719fdf50b02fe34e1e2bd4eccce98
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 16:31:24 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	short i;
+	long f = 1, s = 2, n = f + s;
+
+	printf("%ld, %ld", f, s);
+	for (i = 1; i < 97; i++)
+	{
+		if (i != 97)
+			printf(", ");
+		printf("%ld", n);
+		f = s;
+		s = n;
+		n = f + s;
+	}
+	putchar('\n');
+	return (0);
+}
