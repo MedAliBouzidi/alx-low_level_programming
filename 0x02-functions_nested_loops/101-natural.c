54cb314d938b680683c9f174e551480fe1085139
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 15:47:19 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void);
+{
+	short i, s;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			s += i;
+	}
+	printf("%d", s);
+	putchar('\n');
+	return (0);
+}
