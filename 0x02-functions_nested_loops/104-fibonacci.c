e2cf45e915237133190733225863fbfd94a08a89
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 16:55:11 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,15 @@
 int main(void)
 {
 	short i;
-	long f = 1, s = 2, n = f + s, limit = 100000000000;
-
+	unsigned long int f = 1, s = 2, n = f + s, limit = 100000000000;
+	
 	printf("%ld, %ld", f, s);
 	for (i = 1; i < 97; i++)
 	{
 		if (i != 97)
 			printf(", ");
 		if (n < limit)
-			printf("%ld", n);
-		else
-			printf("%ld%ld", n / limit, n % limit);
+			printf("%lu", n);
 		f = s;
 		s = n;
 		n = f + s;
