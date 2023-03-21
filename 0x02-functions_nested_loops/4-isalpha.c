488b60a200d1ec10ea841ef99ce9b091fb95b65d
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 09:43:25 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for character alphabet or not
+ *
+ * @c: The character ascii code
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
