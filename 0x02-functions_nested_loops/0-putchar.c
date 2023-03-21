c8a776744f90715010875e3adcad5b194c460174
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 08:02:04 -0700

    task 0 & clean up

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char* str = "_putchar";
+
+	for (i = 0; i < strlen(str); i++)
+		_putchar(str[i]);
+	_putchar('\n');
+        return (0);
+}
