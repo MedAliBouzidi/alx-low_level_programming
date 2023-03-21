2c4a60083a11d3b97c45f7b8789513f8dd28310a
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 14:39:17 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add to integers
+ *
+ * @a: first inputed integer
+ * @b: second inputed integer
+ *
+ * Return: the sum of the a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
