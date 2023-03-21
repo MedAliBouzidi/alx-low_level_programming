0e0d98b3adde2b66608377310d8f9d4627a7fa06
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 14:57:15 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,13 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n = 98)
 	{
-		for (i = n; i <= 98; i++)
+		printf("%d");
+	}
+	else if (n < 98)
+	{
+		for (i = n + 1; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -23,7 +27,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n - 1; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
