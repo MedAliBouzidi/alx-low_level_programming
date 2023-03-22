2db5a2189316542c314b05b12537b1092342294c
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 18:40:10 -0700

    fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	unsigned long h1, h2;
 
 	printf("%ld, %ld, ", f, s);
-	for (i = 3; i < 44; i++)
+	for (i = 0; i < 45; i++)
 	{
 		n = f + s;
 		printf("%lu, ", n);
@@ -24,7 +24,7 @@ int main(void)
 	sh1 = s / 1000000000;
 	fh2 = f % 1000000000;
 	sh2 = s % 1000000000;
-	for (i = 44; i < 99; i++)
+	for (i = 45; i < 99; i++)
 	{
 		h1 = fh1 + sh1;
 		h2 = fh2 + sh2;
