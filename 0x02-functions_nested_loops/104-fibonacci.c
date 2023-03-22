6ff03afce76a06183525de8e7e149d972c70e8cb
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 20:17:50 -0700

    fixing

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,40 +7,31 @@
  */
 int main(void)
 {
-	short i;
-	unsigned long f = 1, s = 2, n;
-	unsigned long fh1, fh2, sh1, sh2;
-	unsigned long h1, h2;
+	unsigned long f = 1, s = 2, tmp1, max = 100000000, fo = 0, so = 0, tmp2;
+	short int i = 1, initial0s;
 
-	printf("%ld, %ld, ", f, s);
-	for (i = 0; i < 45; i++)
+	while (i <= 98)
 	{
-		n = f + s;
-		printf("%lu, ", n);
-		f = s;
-		s = n;
-	}
-	fh1 = f / 1000000000;
-	sh1 = s / 1000000000;
-	fh2 = f % 1000000000;
-	sh2 = s % 1000000000;
-	for (i = 45; i < 99; i++)
-	{
-		h1 = fh1 + sh1;
-		h2 = fh2 + sh2;
-		if (h2 > 999999999)
+		if (fo > 0)
+		printf("%lu", fo);
+		initial0s = numLength(max) - 1 - numLength(f);
+		while (fo > 0 && initial0s > 0)
 		{
-			h1++;
-			h2 %= 1000000000;
+			printf("%i", 0);
+			initial0s--;
 		}
-		printf("%lu%lu", h1, h2);
+		printf("%lu", f);
+		tmp1 = (f + s) % max;
+		tmp2 = fo + so + (f + s) / max;
+		f = s;
+		fo = so;
+		s = tmp1;
+		so = tmp2;
 		if (i != 98)
 			printf(", ");
-		fh1 = sh1;
-		fh2 = sh2;
-		sh1 = h1;
-		sh2 = h2;
+		else
+			printf("\n");
+		i++;
 	}
-	putchar('\n');
 	return (0);
 }
