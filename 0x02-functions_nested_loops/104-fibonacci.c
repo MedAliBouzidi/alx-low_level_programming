a57708eb305065e703700cce4542a7c6a5d52164
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-21 20:22:03 -0700

    fixing

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 /**
+ * numLength - returns the length of string
+ * @num : operand number
+ * Return: number of digits
+ */
+int numLength(unsigned long num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0
