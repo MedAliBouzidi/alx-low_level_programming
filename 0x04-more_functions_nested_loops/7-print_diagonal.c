f9a4605bc0121c78cb6f8321619ce6eb5d8b4218
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-23 09:29:21 -0700

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,18 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0, j;
+	int i, j;
 
-	while (i < n)
-	{
-		for (j = 0; j < i; j++)
-			_putchar(32);
-		_putchar(92);
+	if (n <= 0)
 		_putchar('\n');
-		i++;
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(32);
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
