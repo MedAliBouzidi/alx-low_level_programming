78f1bb1dc983cc2e0f1d543b5f64e30a8ab640b7
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-23 07:46:46 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is uppercase or not
+ *
+ * @c; Charctere ascii code
+ *
+ * Return: 1 when c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
