90540d7ab33093d5be58ac0c0e72da6e9e5f4281
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-23 10:11:19 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle with #
+ *
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - 1 - i; j++)
+				_putchar(32);
+			for (k = 0; k <= i; k++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+}
