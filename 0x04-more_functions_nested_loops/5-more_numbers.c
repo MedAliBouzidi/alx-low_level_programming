634eac04d9859a9deafa7de171062a636bcbe720
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-23 08:44:01 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * mosre_numbers - print 10 times from 0 to 14
+ */
+void more_numbers(void)
+{
+	int c, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 48; c <= 62; c++)
+		{
+			if (c <= 57)
+				_putchar(c);
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
