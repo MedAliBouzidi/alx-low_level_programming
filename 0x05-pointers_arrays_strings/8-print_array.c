fcb99c51485fb34f2ae59d5f5f8d6593bae3041b
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-27 14:47:40 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print array of integers
+ *
+ * @a: array of integers
+ * @n: size of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
