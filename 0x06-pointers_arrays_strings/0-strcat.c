f62c4508296c28c2b057412edf099cd29d1c0f5a
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-29 06:55:13 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: pointer to dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int ld, ls;
+
+	ld = ls = 0;
+
+	while (*(dest + ld))
+		ld++;
+	while (*(src + ls))
+	{
+		dest[ld + ls] = src[ls];
+		ls++;
+	}
+	src[ls] = '\0';
+	return (dest);
+}
