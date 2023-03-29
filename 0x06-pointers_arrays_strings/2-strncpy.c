64edba9be37bb2d458bf3c48c851d0e26c351beb
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-29 07:33:07 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy at most n bytes of string src
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ *
+ * Return: pointer to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	
+	while (i < n && *(src + i))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i <= n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
