6d298981f45dcc52d241590a8cc8d6221cb9e243
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-03-29 10:18:52 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,18 @@
 char *cap_string(char *s)
 {
 	int i = 0, j, sep[14] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 124};
+	if (s[i] >= 97 && s[i] <= 122)
+		s[i] = s[i] - 32;
 
-	while (*(s + i))
+	while (*(s + i + 1))
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[i + 1] >= 97 && s[i + 1] <= 122)
 		{
 			j = 0;
 			while (*(sep + j))
 			{
-				if (s[i - 1] == sep[j])
-					s[i] -= 32;
+				if (s[i] == sep[j])
+					s[i + 1] -= 32;
 				j++;
 			}
 		}
