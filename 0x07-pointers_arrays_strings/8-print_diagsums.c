df05b6c7e19e3d3271c39855923afdfa5044a708
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-03 16:03:18 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,14 +12,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, s1, s2;
+	int i, j, s1, s2;
 
-	i = s1 = s2 = 0;
+	i = j = s1 = s2 = 0;
 	while (i < size)
 	{
 		s1 += *(a + (i * size + i));
-		s2 += *(a + (i * 2 + size - 1));
+		s2 += *(a + (size - 1 + j));
 		i++;
+		j += size - 1;
 	}
 
 	printf("%d, %d\n", s1, s2);
