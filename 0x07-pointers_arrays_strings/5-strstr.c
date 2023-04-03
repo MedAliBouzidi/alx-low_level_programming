9171ddd57dfd73abdaa8afb1ce52662a9df1e6f6
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-03 09:57:44 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ *
+ * @haystack: main string
+ * @needle: substring
+ *
+ * Return: pointer to the beginning of the located substring,
+ * otherwise NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j;
+
+	while (*(haystack + i))
+	{
+		j = 0;
+		while (*(needle + j) && *(needle + j) != *(haystack + i))
+			j++;
+		if (*(needle + j) == *(haystack + i))
+		{
+			j = 0;
+			while (*(haystack + i + j) && *(haystack + i + j) == *(needle + j))
+				j++;
+			if (!*(needle + j))
+				return (haystack + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
