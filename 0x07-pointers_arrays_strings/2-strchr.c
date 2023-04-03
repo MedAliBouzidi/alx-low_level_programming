ff4eec98b94e02ae6328f37a3259abe5beabe47f
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-03 09:00:50 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @s: pointer to string
+ * @c: character to locate
+ *
+ * Return: pointer to first occurrence of c if exists
+ * NULL otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
