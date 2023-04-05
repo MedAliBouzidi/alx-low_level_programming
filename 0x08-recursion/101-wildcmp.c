21ef8e9facb533ba7a59cd11e6cdb99fd739d11f
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-05 10:19:08 -0700

    task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two string
+ *
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if the strings can be considered identical,
+ * otherwise return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+                return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
