4bb8eadd7f2861a810f4aea060324dd541e8fa65
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-05 07:33:55 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string 
+ *
+ * @s: string to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
