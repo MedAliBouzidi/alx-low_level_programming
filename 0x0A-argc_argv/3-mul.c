d642b3fcd46db19a2ed98f1cfe891d4a63a912e1
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-08 10:08:42 -0700

    fix task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+int _atoi(char *s);
+
 /**
  * main - Entry point
  *
@@ -19,9 +21,41 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	a = *(*(argv + 1)) - 48;
-	b = *(*(argv + 2)) - 48;
+	a = _atoi(*(argv + 1));
+	b = _atoi(*(argv + 2));
 
 	printf("%d\n", a * b);
 	return (0);
+}
+
+/**
+ * _atoi - convert string into integer
+ *
+ * @s: pointer to string
+ *
+ * Return: the extracted number otherwise 0
+ */
+int _atoi(char *s)
+{
+	short found;
+	int l, minus;
+	unsigned int final;
+
+	l = found = final = 0;
+	minus = 1;
+	while (*(s + l))
+	{
+		if (*(s + l) == '-')
+			minus *= -1;
+		if (*(s + l) >= 48 && *(s + l) <= 57)
+		{
+			final *= 10;
+			final += (*(s + l) - 48);
+			found = 1;
+		}
+		else if (found == 1)
+			break;
+		l++;
+	}
+	return (final * minus);
 }
