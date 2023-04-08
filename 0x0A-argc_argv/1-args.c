dfbbcf1d477d20a8cf0e0a50f6e5d6d78184e848
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-08 09:30:24 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	argv = argv;
+	printf("%d\n", argc);
+	return (0);
+}
