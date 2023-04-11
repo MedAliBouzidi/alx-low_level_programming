33d5bb6d85ee06d2f9fe5d578793d2b5d27dbfa9
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-11 09:52:06 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i = 0;
 	
+	if (size == 0)
+		return (NULL);
 	a = malloc(size * sizeof(char));
 	if (a == NULL)
 		return (NULL);
