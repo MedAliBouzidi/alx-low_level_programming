ad8c5e6fb271a888c507f7bb0247535f873753c9
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-14 15:49:03 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,6 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p;
 	unsigned int i;
 
+	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
