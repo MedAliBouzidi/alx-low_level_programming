b6bc675ec3c8fcf1c1a94d58aac7ac705086f82d
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-21 03:20:36 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 		if (i++ != n - 1)
-			printf("%s", *separator);
+			printf("%s", separator);
 	}
 
 	va_end(ap);
