e8883b1432ae1bb007c7311260e186279c77490c
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-04-29 11:04:36 -0700

    fix styling

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * *add_nideint_end - adds a new node at the end of a listint_t list
+ * *add_nodeint_end - adds a new node at the end of a listint_t list
  *
  * @head: pointer to the address of head of list
  * @n: element to adds to list
