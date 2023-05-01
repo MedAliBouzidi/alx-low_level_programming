cf4c2ca5e75ba9cfefaa93cc071fc62da18a12d4
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-05-01 10:16:23 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	h = *head;
-	while (i++ < idx && h != NULL)
+	while (i++ < idx && h->next != NULL)
 	{
 		prev = h;
 		h = h->next;
