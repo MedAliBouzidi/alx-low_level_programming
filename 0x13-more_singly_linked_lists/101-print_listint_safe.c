145ea9282b7f5f5cf0d5eb8d934cee95b2078e84
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-05-02 13:35:33 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,13 +15,12 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		n++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (head - head->next > 0)
-			head = head->next;
-		else
+		if (head - head->next <= 0)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
+		head = head->next;
 	}
 
 	return (n);
