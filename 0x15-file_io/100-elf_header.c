4899933da3760155e78b1263d55ea1b3dd102fcb
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-05-08 07:13:26 -0700

    init 101-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - displays the information contained
+ * in the ELF header at the start of an ELF file.
+ *
+ * @argc: argument count
+ * @argv: argument values
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int main(int argc, char **argv)
+{
+	return (0);
+}
