89e5503e0896ff429220f67c4f4c5d78399f3493
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-05-08 06:10:11 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		write_res = write(f, text_content, len);
 		if (write_res == -1)
 		{
-			write(1, "fails", 0);
+			write(1, "fails", 6);
 			return (-1);
 		}
 	}
