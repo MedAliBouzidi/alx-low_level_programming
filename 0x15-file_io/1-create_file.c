9846c740410eff73dc76e5d8cf7a5cd8d469fbb8
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-05-08 06:09:05 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,41 @@
 #include "main.h"
 
 /**
+ * create_file - creates a file
  *
+ * @filename: name of new created file
+ * @text_content: initial content of the file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…) or filename is NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f, len;
+	ssize_t write_res;
+
+	if (filename == NULL)
+		return (-1);
+
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		len = 0;
+		while (*(text_content + len++))
+			;
+
+		write_res = write(f, text_content, len);
+		if (write_res == -1)
+		{
+			write(1, "fails", 0);
+			return (-1);
+		}
+	}
+
+	close(f);
+
+	return (1);
+}
